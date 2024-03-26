3b1c288919401fa259f79caad02dcc3fc1b53f89
Anjana <anjanaathul1997@gmail.com>
2024-03-26 14:03:01 -0400

    CalculatorFunctions.java file

---
+++ CalculatorFunctions.java
@@ -0,0 +1,24 @@
+package mavendemo;
+
+public class CalculatorFunctions {
+	public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double divide(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            System.out.println("Error: Cannot divide by zero");
+            return Double.NaN; // NaN - Not a Number
+        }
+    }
+}
